97ca4115327a78095f69a5983e7d8864b993a554
Matheus <matheus150101miranda@gmail.com>
2024-11-27 21:01:43 -0300

    feat: add styles to MenuFull

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,6 +5,7 @@ import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { useState } from 'react'
 import Logo from '../Logo'
 import * as S from './styles'
+import Button from '../Button'
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -26,6 +27,20 @@ const Menu = () => {
       </S.MenuGroup>
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign Up">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
   )
--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -7,3 +7,9 @@ export default {
 } as Meta
 
 export const Default: StoryObj = {}
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
