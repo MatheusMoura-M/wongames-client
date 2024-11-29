7d5fb4d8e92da5f76870daa3b29d4a4c33fb3ab5
Matheus <matheus150101miranda@gmail.com>
2024-11-28 22:35:10 -0300

    feat: add Ribbon structure and tests

---
+++ src/components/Ribbon/stories.tsx
@@ -0,0 +1,29 @@
+import { StoryObj, Meta } from '@storybook/react'
+import Ribbon, { RibbonProps } from '.'
+
+export default {
+  title: 'Ribbon',
+  component: (args) => (
+    <div
+      style={{
+        width: '40rem',
+        height: '25rem',
+        position: 'relative',
+        backgroundColor: '#888'
+      }}
+    >
+      <Ribbon {...args} />
+    </div>
+  )
+} as Meta
+
+export const Default: StoryObj<RibbonProps> = {
+  args: {
+    children: 'Best Seller'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+}
