729370f9c166b4843eb1a59d7e95e8e44c2b0591
Matheus <matheus150101miranda@gmail.com>
2024-12-07 09:09:16 -0300

    feat: add GameCard structure and initial tests

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,21 @@
+import { StoryObj, Meta } from '@storybook/react'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00'
+  }
+} as Meta
+
+export const Default: StoryObj<GameCardProps> = {
+  render: (args) => (
+    <div style={{ width: '30rem' }}>
+      <GameCard {...args} />
+    </div>
+  )
+}
