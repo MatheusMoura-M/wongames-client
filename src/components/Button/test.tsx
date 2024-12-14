449df07a0a154da5e4d3c85038748fff57668d96
Matheus <matheus150101miranda@gmail.com>
2024-12-14 11:33:35 -0300

    feat: Add Button minimal

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -50,6 +50,25 @@ describe('<Button />', () => {
     expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 
+  it('should render a minimal version', () => {
+    renderWithTheme(<Button minimal>Buy now</Button>)
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyleRule(
+      'color',
+      '#F231A5'
+    )
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyleRule(
+      'background',
+      'none'
+    )
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyleRule(
+      'background',
+      'none',
+      {
+        modifier: ':hover'
+      }
+    )
+  })
+
   it('should render Button as a link', () => {
     renderWithTheme(
       <Button as="a" href="/link">
