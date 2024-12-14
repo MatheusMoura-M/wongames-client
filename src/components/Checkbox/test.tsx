63c2abed0c73932d81d80d1b91f4398aeb132257
Matheus <matheus150101miranda@gmail.com>
2024-12-14 15:00:23 -0300

    feat: Add onChange method to Checkbox

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,7 +1,9 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import { renderWithTheme } from '@/utils/tests/helpers'
-import Checkbox from '.'
+import userEvent from '@testing-library/user-event'
+
 import theme from '@/styles/theme'
+import Checkbox from '.'
 
 describe('<Checkbox />', () => {
   it('should render with label', () => {
@@ -25,6 +27,18 @@ describe('<Checkbox />', () => {
     )
     expect(screen.getByText(/checkbox label/i)).toHaveStyle({
       color: theme.colors.black
+    })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck} />)
+
+    expect(onCheck).not.toHaveBeenCalled()
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
     })
   })
 })
