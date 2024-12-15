3bb94a08e384392ec9a3654429b6cdfbe4d529b8
Matheus <matheus150101miranda@gmail.com>
2024-12-15 10:35:39 -0300

    feat: Add Auth Desktop styles

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,4 +1,3 @@
-
 import Heading from '@/components/Heading'
 import Logo from '@/components/Logo'
 import * as S from './styles'
@@ -10,20 +9,30 @@ type AuthProps = {
 
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
-     <S.BannerBlock>
-      <Logo />
-      <Heading>All your favorite games in one place</Heading>
-      <S.Subtitle>
-        <strong>WON</strong> is the best and most complete gaming platform.
-      </S.Subtitle>
-      <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+    <S.BannerBlock>
+      <S.BannerContent>
+        <Logo />
+
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <S.Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </S.Subtitle>
+        </div>
+
+        <S.Footer>Won Games 2020 © Todos os Direitos Reservados</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
+
     <S.Content>
-      <Logo color="black" size="large" />
-      <Heading color="black" lineColor="secondary" lineLeft>
-        {title}
-      </Heading>
-      {children}
+      <S.ContentWrapper>
+        <Logo color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 )
