2dbaea49aac30060e95f901da1ad345d419e167d
Matheus <matheus150101miranda@gmail.com>
2024-12-21 15:44:07 -0300

    feat: Add argTypes to platforms on GameDetails

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,6 +1,6 @@
 
 import { Meta, StoryObj } from '@storybook/react'
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
 
 export default {
   title: 'Game/GameDetails',
@@ -9,13 +9,24 @@ export default {
     backgrounds: {
       default: 'won-dark'
     }
+  },
+  args: {
+    platforms: ['windows', 'linux', 'mac']
+  },
+  argTypes: {
+    platforms: {
+      control: {
+        type: 'inline-check',
+      },
+      options: ['windows', 'linux', 'mac']
+    }
   }
 } as Meta
 
-export const Default: StoryObj = {
-  render: () => (
+export const Default: StoryObj<GameDetailsProps> = {
+  render: (args) => (
     <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
-      <GameDetails platforms={['windows', 'linux', 'mac']} />
+      <GameDetails {...args} />
     </div>
   )
 }
