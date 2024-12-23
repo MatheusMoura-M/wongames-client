d71a051fb518a72248a92abafa4f3863776ea9ca
Matheus <matheus150101miranda@gmail.com>
2024-12-23 15:01:15 -0300

    feat: Add Empty to Wishlist

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -8,6 +8,7 @@ import { Grid } from '../Grid'
 
 import * as S from './styles'
 import { Divider } from '@/components/Divider'
+import Empty from '@/components/Empty'
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[]
@@ -16,7 +17,7 @@ export type WishlistTemplateProps = {
 }
 
 const Wishlist = ({
-  games,
+  games = [],
   recommendedGames,
   recommendedHighlight
 }: WishlistTemplateProps) => (
@@ -26,11 +27,19 @@ const Wishlist = ({
         Wishlist
       </Heading>
 
-      <Grid>
-        {games?.map((game, index) => (
-          <GameCard key={`wishlist-${index}`} {...game} />
-        ))}
-      </Grid>
+      {games.length ? (
+        <Grid>
+          {games?.map((game, index) => (
+            <GameCard key={`wishlist-${index}`} {...game} />
+          ))}
+        </Grid>
+      ) : (
+        <Empty
+          title="Your wishlist is empty"
+          description="Games added to your wishlist will appear here"
+          hasLink
+        />
+      )}
 
       <Divider />
     </Container>
