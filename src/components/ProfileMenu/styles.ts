6347c2a6ef6b3058e117e279b53b6b4c8c632c69
Matheus <matheus150101miranda@gmail.com>
2024-12-24 14:33:02 -0300

    feat: Add ProfileMenu styles and tests

--- src/components/ProfileMenu/styles.ts
+++ src/components/ProfileMenu/styles.ts
@@ -1,5 +1,51 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Nav = styled.nav``
+export const Nav = styled.nav`
+  ${({ theme }) => css`
+    display: flex;
+    border-bottom: 0.1rem solid ${theme.colors.lightGray};
 
-export const Link = styled.div``
+    > a {
+      flex: 1;
+      text-decoration: none;
+    }
+
+    ${media.greaterThan('medium')`
+      flex-direction: column;
+      border: 0;
+
+      a:not(:last-child) {
+        border-bottom: 0.1rem solid ${theme.colors.lightGray};
+      }
+    `}
+  `}
+`
+
+export const Link = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+    color: ${theme.colors.black};
+    display: flex;
+    align-items: center;
+    padding: ${theme.spacings.xsmall} ${theme.spacings.small};
+    transition: background, color, ${theme.transition.default};
+
+    &:hover {
+      background: ${theme.colors.primary};
+      color: ${theme.colors.white};
+    }
+
+    > span {
+      margin-left: ${theme.spacings.xsmall};
+    }
+
+    ${media.lessThan('medium')`
+      justify-content: center;
+
+      > span {
+        display: none;
+      }
+    `}
+  `}
+`
--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import ProfileMenu from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
 
 describe('<ProfileMenu />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<ProfileMenu />)
-    expect(screen.getByRole('heading', { name: /ProfileMenu/i })).toBeInTheDocument()
+  it('should render the menu', () => {
+    const { container } = renderWithTheme(<ProfileMenu />)
 
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
     expect(container.firstChild).toMatchSnapshot()
   })
 })
