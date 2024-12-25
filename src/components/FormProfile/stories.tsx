dea8cd572b9ddea9c51da597caba9d896b714731
Matheus <matheus150101miranda@gmail.com>
2024-12-25 09:45:12 -0300

    feat: Add FormProfile styles

--- src/components/FormProfile/stories.tsx
+++ src/components/FormProfile/stories.tsx
@@ -2,8 +2,14 @@ import { Meta, StoryObj } from '@storybook/react'
 import FormProfile from '.'
 
 export default {
-  title: 'FormProfile',
+  title: 'Form/FormProfile',
   component: FormProfile
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+  render: () => (
+    <div style={{ maxWidth: 860, margin: 'auto' }}>
+      <FormProfile />
+    </div>
+  )
+}
