7048b4927c106829fdf7ba29d0919442c7acdb82
Matheus <matheus150101miranda@gmail.com>
2024-12-25 14:12:27 -0300

    feat: Add useRouter to activeLink and tests

--- src/templates/Profile/index.tsx
+++ src/templates/Profile/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { Container } from '@/components/Container'
 import Heading from '@/components/Heading'
 import ProfileMenu from '@/components/ProfileMenu'
@@ -6,19 +7,22 @@ import { ChildrenProps } from '@/utils/tests/helpers'
 
 import * as S from './styles'
 
-const Profile = ({ children }: ChildrenProps) => (
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        My profile
-      </Heading>
+const Profile = ({ children }: ChildrenProps) => {
+  const { asPath } = useRouter()
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My profile
+        </Heading>
 
-      <S.Main>
-        <ProfileMenu />
+        <S.Main>
+          <ProfileMenu activeLink={asPath} />
+          <S.Content>{children}</S.Content>
+        </S.Main>
+      </Container>
+    </Base>
+  )
+}
 
-        <S.Content>{children}</S.Content>
-      </S.Main>
-    </Container>
-  </Base>
-)
 export default Profile
