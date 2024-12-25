992d58b4c23ba8a7f53b03ab0869899b89722143
Matheus <matheus150101miranda@gmail.com>
2024-12-25 14:04:47 -0300

    feat: Add OrdersList

---
+++ src/components/OrdersList/stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import OrdersList, { OrdersListProps } from '.'
+
+import itemsMock from './mock'
+
+export default {
+  title: 'Profile/OrdersList',
+  component: OrdersList,
+  args: {
+    items: itemsMock
+  }
+} as Meta
+
+export const Default: StoryObj<OrdersListProps> = {
+  render: (args) => (
+    <div style={{ maxWidth: 850, margin: 'auto' }}>
+      <OrdersList {...args} />
+    </div>
+  )
+}
