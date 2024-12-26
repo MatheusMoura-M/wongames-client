ca1f8063e1c6b57cdbb3eed6b435acefdeb22c6c
Matheus <matheus150101miranda@gmail.com>
2024-12-26 10:19:06 -0300

    fix: adjustments

--- src/components/GameCardSlider/mock.ts
+++ src/components/GameCardSlider/mock.ts
@@ -1,41 +1,41 @@
 export default [
   {
-    title: 'Population Zero',
+    title: 'Population Zero 1',
     developer: 'Rockstar Games',
     img: '/img/red-dead-img.jpg',
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 215,00'
   },
   {
-    title: 'Population Zero',
+    title: 'Population Zero 2',
     developer: 'Rockstar Games',
     img: 'https://s2-g1.glbimg.com/_zAZeVzS1-Mpgxb29RN_Rpd1FcU=/0x0:1024x741/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/Q/Y/fNZWBPR1WtXPavRZx8Mw/whatsapp-image-2021-07-20-at-13.31.23.jpeg',
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 215,00'
   },
   {
-    title: 'Population Zero',
+    title: 'Population Zero 3',
     developer: 'Rockstar Games',
     img: '/img/red-dead-img.jpg',
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 215,00'
   },
   {
-    title: 'Population Zero',
+    title: 'Population Zero 4',
     developer: 'Rockstar Games',
     img: 'https://s2-g1.glbimg.com/_zAZeVzS1-Mpgxb29RN_Rpd1FcU=/0x0:1024x741/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/Q/Y/fNZWBPR1WtXPavRZx8Mw/whatsapp-image-2021-07-20-at-13.31.23.jpeg',
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 215,00'
   },
   {
-    title: 'Population Zero',
+    title: 'Population Zero 5',
     developer: 'Rockstar Games',
     img: '/img/red-dead-img.jpg',
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 215,00'
   },
   {
-    title: 'Population Zero',
+    title: 'Population Zero 6',
     developer: 'Rockstar Games',
     img: 'https://s2-g1.glbimg.com/_zAZeVzS1-Mpgxb29RN_Rpd1FcU=/0x0:1024x741/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/Q/Y/fNZWBPR1WtXPavRZx8Mw/whatsapp-image-2021-07-20-at-13.31.23.jpeg',
     price: 'R$ 235,00',
--- src/templates/Games/styles.ts
+++ src/templates/Games/styles.ts
@@ -5,9 +5,9 @@ import { Container } from '@/components/Container'
 export const Main = styled(Container)`
   ${({ theme }) => css`
     ${media.greaterThan('medium')`
-    display: grid;
-    grid-template-columns: 26rem 1fr;
-    gap: ${theme.grid.gutter};
+      display: grid;
+      grid-template-columns: 26rem 1fr;
+      gap: ${theme.grid.gutter};
     `}
   `}
 `
