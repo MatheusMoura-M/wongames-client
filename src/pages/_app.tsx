7a7acc2033a3e94975d5e8f26fe37170e2fc6bca
Matheus <matheus150101miranda@gmail.com>
2024-12-26 17:04:45 -0300

    feat: Add ApolloProvider and first useQuery

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,50 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-
-import theme from '@/styles/theme'
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql
+} from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from '@/styles/global'
+import theme from '@/styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="The best Game Stores in the world!" />
-      </Head>
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
 
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="The best Game Stores in the world!"
+          />
+        </Head>
+
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
