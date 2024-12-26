5b0af4322a441840bdca685dbddfc51453ae0464
Matheus <matheus150101miranda@gmail.com>
2024-12-26 10:44:39 -0300

    feat: Update CartList to have hasButton option

--- src/components/CartList/stories.tsx
+++ src/components/CartList/stories.tsx
@@ -1,4 +1,3 @@
-
 import { Meta, StoryObj } from '@storybook/react'
 import CartList, { CartListProps } from '.'
 
@@ -27,6 +26,14 @@ export const Default: StoryObj<CartListProps> = {
   render: (args) => (
     <div style={{ maxWidth: 800 }}>
       <CartList {...args} />
+    </div>
+  )
+}
+
+export const WithButton: StoryObj<CartListProps> = {
+  render: (args) => (
+    <div style={{ maxWidth: 800 }}>
+      <CartList {...args} hasButton />
     </div>
   )
 }
