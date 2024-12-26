f4f6d3b703de8f23930498a61bff57395e425060
Matheus <matheus150101miranda@gmail.com>
2024-12-26 16:54:43 -0300

    feat: Create ApolloClient directly to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+
 import Home, { HomeTemplateProps } from '@/templates/Home'
 import bannersMock from '@/components/BannerSlider/mock'
 import gamesMock from '@/components/GameCardSlider/mock'
 import highlightMock from '@/components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `
+  })
+
   return <Home {...props} />
 }
+
 // ATENÇÃO:
 // os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
 // getStaticProps => gerar estático em build time
