76f690eea598318e9b2064ce9249b1d5eec2ee76
Matheus <matheus150101miranda@gmail.com>
2025-01-11 11:01:08 -0300

    feat: Connect cache from server to client

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -11,7 +11,7 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+  await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
     variables: {
       pagination: {
@@ -23,15 +23,7 @@ export async function getStaticProps() {
   return {
     revalidate: 60,
     props: {
-      games: data.games.map((game) => ({
-        title: game?.name,
-        slug: game?.slug,
-        developer: game?.developers[0]?.name,
-        img: game?.cover
-          ? `http://localhost:1337${game.cover.url}`
-          : `/img/image_empty.png`,
-        price: game?.price
-      })),
+      initialApolloState: apolloClient.cache.extract(),
       filterItems: filterItemsMock
     }
   }
