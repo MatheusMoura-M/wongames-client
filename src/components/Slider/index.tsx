5fb20be4a6bd16c39cad79516db2ba7bd0b9021c
Matheus <matheus150101miranda@gmail.com>
2025-01-18 11:51:03 -0300

    feat: commented no enused vars

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -20,7 +20,9 @@ const Slider: React.ForwardRefRenderFunction<SlickSlider, SliderProps> = (
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const SlickArrowFix: React.FC<CustomArrowWithChildrenProps> = ({
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     currentSlide,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     slideCount,
     children,
     ...props
