c93bef18202e6b7014ee8bc9339b8ddbee46d6f8
Matheus <matheus150101miranda@gmail.com>
2025-01-24 20:40:16 -0300

    feat: error test

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,10 +1,17 @@
-import FormSignUp from '@/components/FormSignUp'
-import Auth from '@/templates/Auth'
+// import FormSignUp from '@/components/FormSignUp'
+// import Auth from '@/templates/Auth'
 
-export default function SignUp() {
-  return (
-    <Auth title="Sign Up">
-      <FormSignUp />
-    </Auth>
-  )
+// export default function SignUp() {
+//   return (
+//     <Auth title="Sign Up">
+//       <FormSignUp />
+//     </Auth>
+//   )
+// }
+import React from 'react'
+
+const SignUpPage = () => {
+  return <div>Game Page</div>
 }
+
+export default SignUpPage
