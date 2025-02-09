7fde9afb69167d5de7d70a66abdbaec1d2a2b43b
Matheus <matheus150101miranda@gmail.com>
2025-02-09 20:26:52 -0300

    feat: adjustments routes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,17 @@
-// import { Container } from '@/components/Container'
-// import Empty from '@/components/Empty'
-// import Base from '@/templates/Base'
+import { Container } from '@/components/Container'
+import Empty from '@/components/Empty'
+import Base from '@/templates/Base'
 
-// export default function Page404() {
-//   return (
-//     <Base>
-//       <Container>
-//         <Empty
-//           title="404: Not found"
-//           description="Ops...this page does not exist. Go back to the store and enjoy our offers."
-//           hasLink
-//         />
-//       </Container>
-//     </Base>
-//   )
-// }
-import React from 'react'
-
-const NotFoundPage = () => {
-  return <div>Game Page</div>
+export default function Page404() {
+  return (
+    <Base>
+      <Container>
+        <Empty
+          title="404: Not found"
+          description="Ops...this page does not exist. Go back to the store and enjoy our offers."
+          hasLink
+        />
+      </Container>
+    </Base>
+  )
 }
-
-export default NotFoundPage
