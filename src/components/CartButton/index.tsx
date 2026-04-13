f97aedcc11d68f229fd71c9cc21477b5b58220de
Matheus <matheus150101miranda@gmail.com>
2026-04-13 16:06:50 -0300

    feat: add CartButton

---
+++ src/components/CartButton/index.tsx
@@ -0,0 +1,28 @@
+import { useCart } from '@/hooks/use-cart'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+import { RemoveShoppingCart } from '@styled-icons/material-outlined/RemoveShoppingCart'
+import Button from '../Button'
+
+type CartButtonProps = {
+  id: string
+}
+
+const CartButton = ({ id }: CartButtonProps) => {
+  const { isInCart, addToCart, removeFromCart } = useCart()
+
+  return (
+    <Button
+      icon={
+        isInCart(id) ? (
+          <RemoveShoppingCart aria-label="Remove from cart" />
+        ) : (
+          <AddShoppingCart aria-label="Add to cart" />
+        )
+      }
+      size="small"
+      onClick={() => (isInCart(id) ? removeFromCart(id) : addToCart(id))}
+    />
+  )
+}
+
+export default CartButton
