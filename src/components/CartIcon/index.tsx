ad512143cd80c9c41373885dd1517e102d45298d
Matheus <matheus150101miranda@gmail.com>
2026-04-15 17:15:03 -0300

    feat: add loading to CartList

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,13 +1,23 @@
 import { useCart } from '@/hooks/use-cart'
 import { ShoppingCart } from '@styled-icons/material-outlined/ShoppingCart'
+import { useEffect, useState } from 'react'
 import * as S from './styles'
 
 const CartIcon = () => {
   const { quantity } = useCart()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true)
+  }, [])
 
   return (
     <S.Wrapper>
-      {quantity > 0 && <S.Badge aria-label="Cart Items">{quantity}</S.Badge>}
+      {mounted && quantity > 0 && (
+        <S.Badge aria-label="Cart Items">{quantity}</S.Badge>
+      )}
+
       <ShoppingCart aria-label="Shopping Cart" />
     </S.Wrapper>
   )
--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -3,6 +3,7 @@ import Empty from '@/components/Empty'
 import GameItem from '@/components/GameItem'
 import { useCart } from '@/hooks/use-cart'
 import Link from 'next/link'
+import Loader from '../Loader'
 import * as S from './styles'
 
 export type CartListProps = {
@@ -10,7 +11,15 @@ export type CartListProps = {
 }
 
 const CartList = ({ hasButton = false }: CartListProps) => {
-  const { items, total } = useCart()
+  const { items, total, loading } = useCart()
+
+  if (loading) {
+    return (
+      <S.Loading>
+        <Loader />
+      </S.Loading>
+    )
+  }
 
   return (
     <S.Wrapper isEmpty={!items.length}>
