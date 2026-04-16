ec4a76d477d02533f10fdff1991537bbe3c1febb
Matheus <matheus150101miranda@gmail.com>
2026-04-16 15:02:38 -0300

    feat: add useMutation to FormSignUp and change TextField to use onInputChange method

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,49 +1,84 @@
+import Button from '@/components/Button'
+import { FormLink, FormWrapper } from '@/components/Form'
+import TextField from '@/components/TextField'
+import { UsersPermissionsRegisterInput } from '@/graphql/generated/globalTypes'
+import { MUTATION_REGISTER } from '@/graphql/mutations/register'
+import { useMutation } from '@apollo/client'
 import { AccountCircle } from '@styled-icons/material-outlined/AccountCircle'
 import { Email } from '@styled-icons/material-outlined/Email'
 import { Lock } from '@styled-icons/material-outlined/Lock'
 import Link from 'next/link'
+import { useState } from 'react'
 
-import Button from '@/components/Button'
-import { FormLink, FormWrapper } from '@/components/Form'
-import TextField from '@/components/TextField'
+const FormSignUp = () => {
+  const [values, setValues] = useState<UsersPermissionsRegisterInput>({
+    username: '',
+    email: '',
+    password: ''
+  })
 
-const FormSignUp = () => (
-  <FormWrapper>
-    <form>
-      <TextField
-        name="name"
-        placeholder="Name"
-        type="name"
-        icon={<AccountCircle />}
-      />
-      <TextField
-        name="email"
-        placeholder="Email"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="Password"
-        type="password"
-        icon={<Lock />}
-      />
-      <TextField
-        name="confirm-password"
-        placeholder="Confirm password"
-        type="password"
-        icon={<Lock />}
-      />
+  const [createUser] = useMutation(MUTATION_REGISTER)
 
-      <Button size="large" fullWidth>
-        Sign up now
-      </Button>
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
 
-      <FormLink>
-        Already have an account? <Link href="/sign-in">Sign in</Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    createUser({
+      variables: {
+        input: {
+          username: values.username,
+          email: values.email,
+          password: values.password
+        }
+      }
+    })
+  }
+
+  return (
+    <FormWrapper>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="username"
+          placeholder="Username"
+          type="text"
+          onInputChange={(v) => handleInput('username', v)}
+          icon={<AccountCircle />}
+        />
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          onInputChange={(v) => handleInput('email', v)}
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          onInputChange={(v) => handleInput('password', v)}
+          icon={<Lock />}
+        />
+        <TextField
+          name="confirm-password"
+          placeholder="Confirm password"
+          type="password"
+          onInputChange={(v) => handleInput('confirm-password', v)}
+          icon={<Lock />}
+        />
+
+        <Button type="submit" size="large" fullWidth>
+          Sign up now
+        </Button>
+
+        <FormLink>
+          Already have an account? <Link href="/sign-in">Sign in</Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignUp
--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -35,8 +35,14 @@ describe('<TextField />', () => {
   it('Changes its value when typing', async () => {
     const user = userEvent.setup()
 
-    const onInput = jest.fn()
-    render(<TextField onInput={onInput} label="TextField" name="TextField" />)
+    const onInputChange = jest.fn()
+    render(
+      <TextField
+        onInputChange={onInputChange}
+        label="TextField"
+        name="TextField"
+      />
+    )
 
     const input = screen.getByRole('textbox')
     const text = 'This is my new text'
@@ -44,18 +50,18 @@ describe('<TextField />', () => {
     await user.type(input, text)
     await waitFor(() => {
       expect(input).toHaveValue(text)
-      expect(onInput).toHaveBeenCalledTimes(text.length)
+      expect(onInputChange).toHaveBeenCalledTimes(text.length)
     })
-    expect(onInput).toHaveBeenCalledWith(text)
+    expect(onInputChange).toHaveBeenCalledWith(text)
   })
 
   it('Does not changes its value when disabled', async () => {
     const user = userEvent.setup()
 
-    const onInput = jest.fn()
+    const onInputChange = jest.fn()
     render(
       <TextField
-        onInput={onInput}
+        onInputChange={onInputChange}
         label="TextField"
         name="TextField"
         disabled
@@ -69,7 +75,7 @@ describe('<TextField />', () => {
     await waitFor(() => {
       expect(input).not.toHaveValue(text)
     })
-    expect(onInput).not.toHaveBeenCalled()
+    expect(onInputChange).not.toHaveBeenCalled()
   })
 
   it('Renders with error', () => {
