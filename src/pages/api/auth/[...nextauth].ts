e252dde0ed8fd1b0f66266396a85d6a4cc73140d
Matheus <matheus150101miranda@gmail.com>
2026-04-16 21:04:50 -0300

    feat: configure NextAuth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,63 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import NextAuth, { Session, User } from 'next-auth'
+import { JWT } from 'next-auth/jwt'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+const authOptions = {
+  pages: {
+    signIn: '/sign-in'
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sign-in',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials) {
+        if (!credentials) return null
+        const { email, password } = credentials
+
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/local`,
+          {
+            method: 'POST',
+            body: new URLSearchParams({ identifier: email, password })
+          }
+        )
+
+        const data = await response.json()
+
+        if (data.user) {
+          return { ...data.user, jwt: data.jwt }
+        } else {
+          return null
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async session({ session, user }: { session: Session; user: User }) {
+      session.jwt = user.jwt
+      session.id = user.id
+
+      return session
+    },
+
+    async jwt({ token, user }: { token: JWT; user: User }) {
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.name = user.username
+        token.jwt = user.jwt
+      }
+
+      return token
+    }
+  }
+}
+
+const Auth = (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, authOptions)
+
+export default Auth
