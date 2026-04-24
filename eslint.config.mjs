6d1f79034903b1efd4557d0c9f807cd47bcdfed5
Matheus <matheus150101miranda@gmail.com>
2026-04-24 14:20:19 -0300

    fix: error to type module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,20 @@ import nextTs from 'eslint-config-next/typescript'
 import prettier from 'eslint-config-prettier'
 import storybook from 'eslint-plugin-storybook'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+
   prettier,
+
   {
     rules: {
       '@typescript-eslint/no-unused-expressions': 'off',
@@ -16,12 +25,14 @@ export default defineConfig([
       'react-hooks/set-state-in-effect': 'off'
     }
   },
+
   {
     files: ['**/test.{ts,tsx}'],
     rules: {
       '@next/next/no-img-element': 'off'
     }
   },
+
   globalIgnores([
     '.next/**',
     'out/**',
@@ -33,11 +44,12 @@ export default defineConfig([
     'node_modules/**',
     '!.storybook',
     '!.jest',
+    'src/stories/**',
     'generators',
     'src/graphql/generated/**',
     'src/graphql/generated-test/**',
-    'src/graphql/**/__generated__/**',
-    'src/stories/**'
+    'src/graphql/**/__generated__/**'
   ]),
+
   ...storybook.configs['flat/recommended']
 ])
