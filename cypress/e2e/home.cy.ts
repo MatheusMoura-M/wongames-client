2438b57c7aeeec7aa2a80df583b0129e908d8043
Matheus <matheus150101miranda@gmail.com>
2026-05-15 10:05:59 -0300

    feat: start getting banner with get, findByRole and within

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,16 @@
 describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/')
+
+    cy.get('.slick-slider')
+      .first()
+      .within(() => {
+        cy.findByRole('heading', {
+          name: /tortura - a pirate's tale$/i
+        })
+        cy.findByRole('link', { name: /buy now/i })
+
+        cy.get('.slick-dots > :nth-child(2) > button').click()
+      })
   })
 })
