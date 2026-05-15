6a1021be51af9b8f67e650df0b938a552cdcc7cc
Matheus <matheus150101miranda@gmail.com>
2026-05-15 11:14:50 -0300

    feat: start e2e tests to Game page

---
+++ cypress/e2e/game.cy.ts
@@ -0,0 +1,20 @@
+describe('Game Page', () => {
+  it('should render game page sections', () => {
+    cy.visit('/game/cyberpunk_2077')
+
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
+      cy.findByText(/^\* Exclusive Digital Comic - Cyberpunk 2077/i).should(
+        'exist'
+      )
+      cy.findByText('$20.99').should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
+
+    // gallery
+    cy.findAllByRole('button', { name: /thumb \-/i }).should(
+      'have.length.gt',
+      0
+    )
+  })
+})
