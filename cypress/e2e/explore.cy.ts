5dca28ce9322eb938638f73738a8ea3273938900
Matheus <matheus150101miranda@gmail.com>
2026-05-16 16:58:55 -0300

    feat: add tests to sort by platform and genre and empty match

--- cypress/e2e/explore.cy.ts
+++ cypress/e2e/explore.cy.ts
@@ -97,4 +97,27 @@ describe('Explore Page', () => {
         cy.shouldBeLessThan(500)
       })
   })
+
+  it('should filter by platform and genre', () => {
+    cy.findByText(/windows/i).click()
+    cy.location('href').should('contain', 'platforms=windows')
+
+    cy.findByText(/linux/i).click()
+    cy.location('href').should('contain', 'platforms=linux')
+
+    cy.findByText(/mac os/i).click()
+    cy.location('href').should('contain', 'platforms=osx')
+
+    cy.findByText(/action/i).click()
+    cy.location('href').should('contain', 'categories=Action')
+  })
+
+  it('should return empty when no games match', () => {
+    cy.findByText(/free/i).click()
+    cy.findByText(/linux/i).click()
+    cy.findByText(/sports/i).click()
+
+    cy.getByDataCy('game-card').should('not.exist')
+    cy.findByText(/We didn't find any games with this filter/i).should('exist')
+  })
 })
