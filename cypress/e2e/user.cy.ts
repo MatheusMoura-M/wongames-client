4d585cb114f6c635cde5f221bc6fa79c8fe38e41
Matheus <matheus150101miranda@gmail.com>
2026-05-16 19:52:56 -0300

    feat: add SignIn e2e test

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -11,4 +11,20 @@ describe('User', () => {
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username).should('exist')
   })
+
+  it('should sign in and sign out', () => {
+    cy.visit('/sign-in')
+
+    cy.findAllByPlaceholderText(/email/i).type('matheus150101miranda@gmail.com')
+    cy.findAllByPlaceholderText(/password/i).type('Matheus123')
+    cy.findByRole('button', { name: /sign in now/i }).click()
+
+    cy.findByText(/matheus/i)
+      .should('exist')
+      .click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/matheus/i).should('not.exist')
+  })
 })
